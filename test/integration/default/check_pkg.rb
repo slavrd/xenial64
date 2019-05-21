ba56da3e6e886b4336b1bb63cfc7ec57cd54ddd3
Slav Donchev <slav.donchev@gmail.com>
2019-05-21 10:46:19 +0300

    add kitchen test

--- test/integration/default/check_pkg.rb
+++ test/integration/default/check_pkg.rb
@@ -1,8 +1,8 @@
 control 'operating_system' do
 
   describe command('lsb_release -a') do
-    ver = ENV['OS_VER']
-    its('stdout') { should match (/#{ver}test/) }
+    ver = ENV['TEST_OS_VER']
+    its('stdout') { should match (/#{ver}/) }
   end
 
 end
