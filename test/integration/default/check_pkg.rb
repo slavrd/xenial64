2a3d7afdd78180ecff80d55ce9be3230b345aa0d
Slav Donchev <slav.donchev@gmail.com>
2019-05-22 09:10:47 +0300

    update inspec test

--- test/integration/default/check_pkg.rb
+++ test/integration/default/check_pkg.rb
@@ -2,7 +2,7 @@ control 'operating_system' do
 
   describe command('lsb_release -a') do
     ver = ENV['TEST_OS_VER']
-    its('stdout') { should match (/#{ver}/) }
+    its('stdout') { should match (/Ubuntu #{ver}/) }
   end
 
 end
