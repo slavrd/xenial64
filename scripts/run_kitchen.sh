8d5383e3a3fb3adf09461c79f21b841fee5a68c2
Slav Donchev <slav.donchev@gmail.com>
2019-05-22 14:04:07 +0300

    remove appending .pre to VC box version before testing

--- scripts/run_kitchen.sh
+++ scripts/run_kitchen.sh
@@ -9,9 +9,8 @@ bundle install || {
 }
 
 # add box to vagrant
-VC_BOX_VER_PRE="$VC_BOX_VER.pre"
-vagrant box add --force --box-version $VC_BOX_VER_PRE $VC_BOX_NAME || {
-    echo "unable to add box: $VC_BOX_NAME ($VC_BOX_VER_PRE) to vagrant" >&2
+vagrant box add --force --box-version $VC_BOX_VER $VC_BOX_NAME || {
+    echo "unable to add box: $VC_BOX_NAME ($VC_BOX_VER) to vagrant" >&2
     exit 1
 }
 
@@ -19,30 +18,18 @@ vagrant box add --force --box-version $VC_BOX_VER_PRE $VC_BOX_NAME || {
 bundle exec kitchen test
 K_RESULT=$?
 
-# clean up vagrant box
-vagrant box remove --force --box-version $VC_BOX_VER_PRE $VC_BOX_NAME
+# in case kitchen test fails
+if [ "$K_RESULT" != 0 ]; then
 
-if [ "$K_RESULT" == 0 ]; then
-
-    # remove .pre form version name
-    echo "Kitchen test passed. Renaming Vagrant Cloud version..."
-    curl -s \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-        https://app.vagrantup.com/api/v1/box/$VC_BOX_NAME/version/$VC_BOX_VER_PRE \
-        --request PUT \
-        --data "{\"version\": {\"version\": \"$VC_BOX_VER\"}}" | [ "$(jq -r '.success')" != false ]
-
-    [ "$?" != 0 ] && echo "fialed renaming version $VC_BOX_VER_PRE to $VC_BOX_VER"
-
-else
+    # remove box from vagrant
+    vagrant box remove --force --box-version $VC_BOX_VER $VC_BOX_NAME
 
     # delete provider if Kitchen test failed
     echo "Kitchen test failed. Removing VC box version provider..."
     curl -s \
         --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
         --request DELETE \
-        https://app.vagrantup.com/api/v1/box/$VC_BOX_NAME/version/$VC_BOX_VER_PRE/provider/virtualbox
+        https://app.vagrantup.com/api/v1/box/$VC_BOX_NAME/version/$VC_BOX_VER/provider/virtualbox
 
 fi
 
